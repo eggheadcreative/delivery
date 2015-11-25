6bc098da63b34ba577764dc61418c56a68213540
Mohana Krishna <mohan@eggheadcreative.com>
2015-11-25 13:01:22 +0530

    changed from sendgrid to mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,8 +24,8 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.mailgun.org',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => ENV['MAILGUN_USERNAME'],
-  :password       => ENV['MAILGUN_PASSWORD'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
   :domain         => 'eggheadcreative.com'
 }
 ActionMailer::Base.delivery_method = :smtp
