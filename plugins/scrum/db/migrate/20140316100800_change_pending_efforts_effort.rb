d7c237a2eb2de058ca3bf6917587c91977a60272
Durga Raghavapurapu <durga@eggheadcreative.com>
2016-04-05 08:25:51 +0530

    Removed scrum plugin

--- plugins/scrum/db/migrate/20140316100800_change_pending_efforts_effort.rb
+++
@@ -1,9 +0,0 @@
-class ChangePendingEffortsEffort < ActiveRecord::Migration
-  def self.up
-    change_column :pending_efforts, :effort, :float
-  end
-
-  def self.down
-    change_column :pending_efforts, :effort, :integer
-  end
-end
--- plugins/scrum/db/migrate/20150222211700_change_sprint_efforts_effort.rb
+++
@@ -1,9 +0,0 @@
-class ChangeSprintEffortsEffort < ActiveRecord::Migration
-  def self.up
-    change_column :sprint_efforts, :effort, :float
-  end
-
-  def self.down
-    change_column :sprint_efforts, :effort, :integer
-  end
-end
